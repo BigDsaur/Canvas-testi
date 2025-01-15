98b2f5de59263945d3d5a2e3d7e532645c3224f8
BigDsaur <joona879@gmail.com>
2025-01-15 13:57:41 +0200

    lisää

--- script.js
+++ script.js
@@ -76,6 +76,18 @@ ctx.moveTo(centerX + 100, centerY - 80)
 // draw right eye
 ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2)
 
+ctx.stroke()
+
+ctx.beginPath();
+    ctx.moveTo(75, 25);
+    ctx.quadraticCurveTo(25, 25, 25, 62.5);
+    ctx.quadraticCurveTo(25, 100, 50, 100);
+    ctx.quadraticCurveTo(50, 120, 30, 125);
+    ctx.quadraticCurveTo(60, 120, 65, 100);
+    ctx.quadraticCurveTo(125, 100, 125, 62.5);
+    ctx.quadraticCurveTo(125, 25, 75, 25);
+    ctx.stroke();
+
 
 ctx.stroke()
 
