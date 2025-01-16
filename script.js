d0a02200caff106c7c113e9a919dab695d326eb0
BigDsaur <joona879@gmail.com>
2025-01-16 11:27:43 +0200

    Seinistä ei mene läpi toimii nuolinäppäimillä

--- script.js
+++ script.js
@@ -50,44 +50,188 @@ const ctx = canvas.getContext('2d')
 // ctx.fill()
 
 // Arc (ympyrät)
-ctx.beginPath()
+// ctx.beginPath()
 
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
 
-// Draw head
-ctx.arc(centerX, centerY, 200, 0, Math.PI * 2)
+// // Draw head
+// ctx.arc(centerX, centerY, 200, 0, Math.PI * 2)
 
-// move to mouth
-ctx.moveTo(centerX + 100, centerY)
+// // move to mouth
+// ctx.moveTo(centerX + 100, centerY)
 
-// draw mouth
-ctx.arc(centerX, centerY, 100, 0, Math.PI, false)
+// // draw mouth
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false)
 
-// move to left eye
-ctx.moveTo(centerX - 60, centerY - 80,)
+// // move to left eye
+// ctx.moveTo(centerX - 60, centerY - 80,)
   
-//draw left eye
-ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2)
+// //draw left eye
+// ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2)
 
-// move to right eye
-ctx.moveTo(centerX + 100, centerY - 80)
+// // move to right eye
+// ctx.moveTo(centerX + 100, centerY - 80)
 
-// draw right eye
-ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2)
+// // draw right eye
+// ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2)
 
-ctx.stroke()
+// ctx.stroke()
 
-ctx.beginPath();
-    ctx.moveTo(75, 25);
-    ctx.quadraticCurveTo(25, 25, 25, 62.5);
-    ctx.quadraticCurveTo(25, 100, 50, 100);
-    ctx.quadraticCurveTo(50, 120, 30, 125);
-    ctx.quadraticCurveTo(60, 120, 65, 100);
-    ctx.quadraticCurveTo(125, 100, 125, 62.5);
-    ctx.quadraticCurveTo(125, 25, 75, 25);
-    ctx.stroke();
+// ctx.beginPath();
+//     ctx.moveTo(75, 25);
+//     ctx.quadraticCurveTo(25, 25, 25, 62.5);
+//     ctx.quadraticCurveTo(25, 100, 50, 100);
+//     ctx.quadraticCurveTo(50, 120, 30, 125);
+//     ctx.quadraticCurveTo(60, 120, 65, 100);
+//     ctx.quadraticCurveTo(125, 100, 125, 62.5);
+//     ctx.quadraticCurveTo(125, 25, 75, 25);
+//     ctx.stroke();
 
 
-ctx.stroke()
+// ctx.stroke()
 
+// animation 1
+
+// const circle = {
+//     x: 200,
+//     y: 200,
+//     size: 30,
+//     dx: 5,
+//     dy: 4
+// }
+
+// function drawCircle() {
+//     ctx.beginPath();
+//     ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+//     ctx.fillStyle = 'orange';
+//     ctx.fill();
+// }
+
+// function update() {
+//     ctx.clearRect(0,0, canvas.width, canvas.height)
+
+//     drawCircle()
+//     // change position
+//     circle.x += circle.dx
+//     circle.y += circle.dy
+
+//     // detect side walls
+//     if(circle.x + circle.size > canvas.width || circle.x - circle.size < 0)  {
+//         circle.dx *= -1; // circle.dx = circle.dx * -1
+//     }
+
+//     if(circle.y + circle.size > canvas.height || circle.y - circle.size < 0)  {
+//         circle.dy *= -1; // circle.dx = circle.dx * -1
+//     }
+
+//     requestAnimationFrame(update)
+// }
+
+
+// animation 2
+
+const image = document.getElementById('source')
+
+const player = {
+    w: 50,
+    h: 70,
+    x: 20,
+    y: 200,
+    speed: 15,
+    dx: 0,
+    dy: 0, 
+};
+
+function drawPlayer() {
+    ctx.drawImage(image, player.x, player.y, player.w,
+        player.h);
+}
+
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function newpos() {
+    player.x += player.dx
+    player.y += player.dy
+
+    detectWalls()
+}
+
+function detectWalls() {
+    // Left wall
+    if (player.x < 0) {
+        player.x = 0
+    }
+
+    // Right wall
+    if (player.x + player.w > canvas.width) {
+        player.x = canvas.width - player.w
+    }
+
+    if (player.y < 0) {
+        player.y = 0
+    }
+
+    if (player.y + player.h > canvas.height) {
+        player.y = canvas.height - player.h
+    }
+
+}
+
+function update() {
+    clear()
+
+    drawPlayer()
+
+    newpos()
+
+    requestAnimationFrame(update)
+}
+
+function moveUp () {
+    player.dy = -player.speed;
+}
+function moveDown () {
+    player.dy = player.speed;
+}
+function moveRight () {
+    player.dx = player.speed;
+}
+function moveLeft () {
+    player.dx = -player.speed;
+}
+
+function keyDown(e) {
+    if(e.key === 'ArrowRight' || e.key === 'Right') {
+        moveRight();
+    }   else if(e.key === 'ArrowLeft' || e.key === 'Left') {
+        moveLeft();
+    }   else if(e.key === 'ArrowUp' || e.key === 'Up') {
+        moveUp();
+    }   else if(e.key === 'ArrowDown' || e.key === 'Down') {
+        moveDown();
+    }
+}
+
+function keyUp(e) {
+    if (
+        e.key == 'Right' ||
+        e.key == 'ArrowRight' ||
+        e.key == 'Left' ||
+        e.key == 'ArrowLeft' ||
+        e.key == 'Up' ||
+        e.key == 'ArrowUp' ||
+        e.key == 'Down' ||
+        e.key == 'ArrowDown'
+    ) {
+        player.dx = 0;
+        player.dy = 0;
+    }
+}
+
+update()
+
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
